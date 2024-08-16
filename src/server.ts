37dd7b44e65771f226e1a6cfa1ff9c9f0a232d60
jessifedalto <jessicafedalto.jf@gmail.com>
2024-08-16 11:05:31 -0300

    feat: confirm participant and confirm trip done

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import cors from "@fastify/cors"
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipants } from "./routes/confirm-participants";
 
 const app = fastify();
 
@@ -13,3 +15,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipants);
+
+app.listen({port: 3333}).then(() =>{
+    console.log("Server running");
+})
